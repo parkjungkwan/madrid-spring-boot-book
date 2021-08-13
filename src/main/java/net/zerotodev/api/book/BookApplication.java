de1842aa1f241f1987b6638f23c3aefc084583db
parkjungkwan <pakjkwan@gmail.com>
2021-08-13 17:19:08 +0900

    17

--- src/main/java/net/zerotodev/api/book/BookApplication.java
+++ src/main/java/net/zerotodev/api/book/BookApplication.java
@@ -1,13 +1,19 @@
 package net.zerotodev.api.book;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootConfiguration
+@ComponentScan
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class})
 public class BookApplication {
 
 	public static void main(String[] args) {
